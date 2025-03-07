cab42617c99395b83aec6acecb45178da77103ac
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:01:15 +0530

    Add files via upload

---
+++ MethodSameParamsDifferentReturnType.java
@@ -0,0 +1,14 @@
+package MethodoOverloading;
+
+class MethodSameParamsDifferentReturnType {
+    int display(int a) {
+        return a * 2;
+    }
+
+    public static void main(String[] args) {
+        MethodSameParamsDifferentReturnType obj = new MethodSameParamsDifferentReturnType();
+        int result = obj.display(10);
+        System.out.println("Result: " + result);
+    }
+}
+

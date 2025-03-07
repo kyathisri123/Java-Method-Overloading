15369ba490869b2a45206c270c9815e5820aaac9
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:00:56 +0530

    Add files via upload

---
+++ MethodSameNameSameParams.java
@@ -0,0 +1,13 @@
+package MethodoOverloading;
+
+class MethodSameNameSameParams {
+    void display(int a) {
+        System.out.println("First display method: " + a);
+    }
+
+    public static void main(String[] args) {
+        MethodSameNameSameParams obj = new MethodSameNameSameParams();
+        obj.display(10);
+    }
+}
+
